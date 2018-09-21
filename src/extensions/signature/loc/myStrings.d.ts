cbbb84ba9b09d50e17173952760d06b3c6c11d37
Dmitriy Kozlov <jusper@yandex.ru>
2018-09-21 16:03:51 +0300

    signature field

---
+++ src/extensions/signature/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ISignatureFieldCustomizerStrings {
+  Title: string;
+}
+
+declare module 'SignatureFieldCustomizerStrings' {
+  const strings: ISignatureFieldCustomizerStrings;
+  export = strings;
+}
